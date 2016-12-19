0f8004958e38927ae9c0d7c6f49ae2c829b15852
esatish <atish165@gmail.com>
2016-12-19 13:02:21 +0530

    video

--- www/js/controllers/MyApp.js
+++ www/js/controllers/MyApp.js
@@ -1,24 +1,27 @@
 
 angular
-    .module('MyApp').config(function ($routeProvider) {	
-		$routeProvider
-    .when("/home", {
-        templateUrl : "taketest.html",
-		 controller: 'testController as ctrl'
-    }) .when("/allreport", {
-        templateUrl : "allreport.html",
-		 controller: 'inputController'
-    }).when("/videoreport", {
-        templateUrl : "reportsample.html",
-		 controller: 'Videoreport'
-    }).when("/videocard1", {
-        templateUrl : "videocard.html",
-		 controller: 'Videoreport'
-    }).when("/view_profile", {
-        templateUrl : "view_profile.html",
-		 controller: 'profilecontroller'
-    }).otherwise({
-			redirectTo:"/home",
+.module('MyApp').config(function ($routeProvider) {	
+	$routeProvider
+	.when('/view', {
+		
+		templateUrl: 'views/sample.html'
+			}).when("/home", {
+			templateUrl : "taketest.html",
+			controller: 'testController as ctrl'
+			}) .when("/allreport", {
+			templateUrl : "allreport.html",
+			controller: 'inputController'
+			}).when("/videoreport", {
+			templateUrl : "reportsample.html",
+			controller: 'Videoreport'
+			}).when("/videocard1", {
+			templateUrl : "videocard.html",
+			controller: 'Videoreport'
+			}).when("/view_profile", {
+			templateUrl : "view_profile.html",
+			controller: 'profilecontroller'
+			}).otherwise({
+			redirectTo:"/view",
 			controller: 'testController as ctrl'
 		});  
-});
+	});	
--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -20,3 +20,28 @@ angular
 	};
 	
 });
+angular
+    .module('MyApp').controller("Reportcontroller", function ($scope) {
+	$scope.labels = ['Dept1', 'Dept2', 'Dept3', 'Dept4', 'Dept5', 'Dept6', 'Dept7'];
+	$scope.series = ['Group A', 'Group B'];
+	$scope.colours = ['#717984', '#F1C40F'];
+	$scope.data = [
+		[65, 59, 80, 81, 56, 55, 40],
+		[28, 48, 40, 19, 86, 27, 90]
+	];
+});
+
+
+
+
+angular
+    .module('MyApp').controller("Report1controller",function ($scope) {
+    $scope.labels = ['Memb1', 'Memb2', 'Memb3', 'Memb4', 'Memb5', 'Memb6', 'Memb7'];
+    $scope.series = ['Test 1', 'Test 2'];
+	$scope.colours = ['#717984', '#F1C40F'];
+	
+    $scope.data = [
+		[65, 59, 80, 81, 56, 55, 40],
+		[28, 48, 40, 19, 86, 27, 90]
+	];
+});
